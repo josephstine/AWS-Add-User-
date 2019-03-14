8ad1975530a92c192e2bbfe2fd9190984be7b6a8
josephstine <josephstine@nevada.unr.edu>
2019-03-14 15:31:06 -0700

    got rid of useradd and usermod, now passing in with echo etc/passwd and etc/group

--- adduser.sh
+++ adduser.sh
@@ -9,15 +9,23 @@ if [ $(id -u) -eq 0 ]; then #if user id is equal to 0/root read the username,ful
 		exit 1
 	else
 		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password) #hash the password using crypt and perl
-		useradd -m -p $pass $username
-		usermod -c "$fullname" $username
+		low=1001
+		high=10000
+		uidandgid=$((low + RANDOM%(1+high-low)))
+		echo $uidandgid
+		etcPasswordLine="$username:x:$uidandgid:$uidandgid:$fullname:/home/$username:/bin/bash"
+		echo $etcPasswordLine >> /etc/passwd
+		etcGroupLine="$username:x:$uidandgid"
+		echo $etcGroupLine >> /etc/group
+		echo "$username:$pass" | chpasswd
+		#useradd -m -p $pass $username
+		#usermod -c "$fullname" $username
 		[ $? -eq 0 ] && echo "User added succesfully" || echo "User couldn't be added!"
 	fi
 else
 	echo "root access required"
 	exit 2
 fi
-
 #copy files from skel to the home of the username, make the directory,mod then, chown
 cp -r /etc/skel/. /home/"$username"
 mkdir -p /home/"$username"
